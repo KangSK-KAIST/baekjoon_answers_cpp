fa4df8b03f3d251d9aa312f02fef4a17038302da
KangSK-KAIST <steelyk7@gmail.com>
2021-01-19 00:11:06 +0900

    Added answer for level 13 - 1

--- src/15649.cpp
+++ src/15649.cpp
@@ -8,6 +8,36 @@
 #include <vector>
 #include <algorithm>
 
+bool inVector(std::vector<int>* pn, int n)
+{
+	for (int i : *pn)
+	{
+		if (i == n)
+			return true;
+	}
+	return false;
+}
+
+void printAll(std::vector<int> n, int N, int M)
+{
+	if ((int)n.size() == M)
+	{
+		for (int i : n)
+			std::cout << i << ' ';
+		std::cout << '\n';
+		return;
+	}
+	for (int i = 1; i <= N; i++)
+	{
+		if (!inVector(&n, i))
+		{
+			std::vector<int> t = n;
+			t.push_back(i);
+			printAll(t, N, M);
+		}
+	}
+}
+
 int main(void)
 {
 	std::cin.tie(NULL);
@@ -16,7 +46,9 @@ int main(void)
 	int N, M;
 	std::cin >> N >> M;
 
-	
+	std::vector<int> n;
+
+	printAll(n, N, M);
 
 	return 0;
 }
